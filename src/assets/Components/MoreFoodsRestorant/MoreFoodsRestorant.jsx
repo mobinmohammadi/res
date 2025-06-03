e3683fc3b45cc703f0ad2f7e91eba40a9a50c295
mobin <mobinmohammadi4321@gmail.com>
2025-06-03 20:10:04 +0330

    sfkj

--- src/assets/Components/MoreFoodsRestorant/MoreFoodsRestorant.jsx
+++ src/assets/Components/MoreFoodsRestorant/MoreFoodsRestorant.jsx
@@ -6,7 +6,7 @@ export default function MoreFoodsRestorant({ title, price, img }) {
     <>
 
       <div>
-        <span className="text-xl text-zinc-700 font-bold">پیتزا آمریکایی</span>
+        <span className="text-xl text-zinc-700 font-bold">پیتزا آمریdکایی</span>
         <MoreFoodsBoxes/>
       </div>
     </>
