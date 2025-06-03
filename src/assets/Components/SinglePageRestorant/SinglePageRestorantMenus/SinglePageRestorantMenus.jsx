28e39e47c6290389896691d580fa24b76aab14b8
mobin <mobinmohammadi4321@gmail.com>
2025-06-03 20:01:55 +0330

    ojfhn

---
+++ src/assets/Components/SinglePageRestorant/SinglePageRestorantMenus/SinglePageRestorantMenus.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import MoreFoodsBoxes from "../../MoreFoodsRestorant/MoreFoodsBoxes/MoreFoodsBoxes";
+
+export default function SinglePageRestorantMenu({
+  searchInMenuRestorant,
+  handleMenuSingleRestoranst,
+  dataSingleResturants,
+  arrayUserBasket,
+  setArrayUserBasket,
+  addToBasketUser,
+  idProductInBasket
+
+  
+}) {
+  const [menusResturants, setMenusResturants] = useState([]);
+  const dataSingleResturantsMenus = dataSingleResturants.menu;
+  // useEffect(() => {
+  //   fetch(`${BaseUrl}/restaurants`)
+  //     .then((res) => res.json())
+  //     .then((result) => {
+  //       setMenusResturants(result);
+  //     });
+  // }, []);
+
+  return (
+      <div className="flex container-custom pb-5 flex-col items-center">
+        <div className="flex w-full relative mr-4 ml-4 xs:mr-12 xs:ml-12  items-center pt-2 pb-2 pr-2 rounded-sm mt-5 mb-5 bg-white">
+          <input
+            // value={valueForSearch}
+            onChange={(e) => handleMenuSingleRestoranst(e)}
+            className="border-0 w-full outline-0"
+            type="text"
+            placeholder="جستوجو در منو ..."
+          />
+          <svg className="cursor-pointer absolute  left-3 w-5 h-5">
+            <use href="#magnifying-glass"></use>
+          </svg>
+        </div>
+        <div className=" grid grid-cols-1 sm:grid-cols-2   xl:grid-cols-3 w-[90%]  gap-3">
+          {searchInMenuRestorant.length
+            ? searchInMenuRestorant.map((menu, index) => (
+                <div key={index + 1} className="">
+                  <MoreFoodsBoxes idProductInBasket={idProductInBasket} arrayUserBasket={arrayUserBasket} addToBasketUser={addToBasketUser} setArrayUserBasket={setArrayUserBasket} menu={menu} />
+                </div>
+              ))
+            : dataSingleResturantsMenus?.map((menu, index) => (
+                <div key={index + 1} className="">
+                  <MoreFoodsBoxes dataSingleResturantsMenus={dataSingleResturantsMenus} idProductInBasket={idProductInBasket} arrayUserBasket={arrayUserBasket} addToBasketUser={addToBasketUser} setArrayUserBasket={setArrayUserBasket}  menu={menu} />
+                </div>
+              ))}
+        </div>
+      </div>
+  );
+}
---
+++ src/assets/Page/SinglePageRestorant/SinglePageRestorant.jsx
@@ -0,0 +1,239 @@
+import React, { useEffect, useRef, useState } from "react";
+import Topbar from "./../../Components/Topbar/Topbar";
+import FooterPc from "./../../Components/FooterPc/FooterPc";
+import HeaderRestorant from "./../../Components/SinglePageRestorant/HeaderRestorant/HeaderRestorant";
+import MoreFoodsRestorant from "../../Components/MoreFoodsRestorant/MoreFoodsRestorant";
+import MoreFoodsBoxes from "../../Components/MoreFoodsRestorant/MoreFoodsBoxes/MoreFoodsBoxes";
+import CommentsSections from "./../../Components//SinglePageRestorant/CommentsSections/CommentsSections";
+import AddressRestorant from "./../../Components//SinglePageRestorant/AddressRestorant/AddressRestorant";
+import { useParams } from "react-router";
+import MenuSinglePageRestorant from "././../../Components//SinglePageRestorant/MenuSinglePageRestorant/MenuSinglePageRestorant";
+import SinglePageRestorantMenu from "./../../Components//SinglePageRestorant/SinglePageRestorantMenus/SinglePageRestorantMenus";
+import RestorantsData from "../../../../RestorantsData.json";
+import { CartProvider } from "../../Components/Context/Context";
+import FoodBoxes from "../../Components/FoodBoxes/FoodBoxes";
+import categuryTitleFoods from "./../../../Data.js";
+import SliderTitleFoods from "../../Components/SinglePageRestorant/SliderTitleFoods/SliderTitleFoods";
+
+export default function SinglePageRestorant() {
+  // const baseUrl = import.meta.env.VITE_BASE_URL;
+
+  const [valueForSearch, setValueForSearch] = useState("");
+  const [allRestorants, setAllRestorants] = useState(RestorantsData);
+  const [countProduct, setCountProduct] = useState(0);
+
+  const paramsID = useParams().ResoruntID;
+  let [dataSingleResturants, setDataSingleResturants] = useState({});
+  const [allComments, setAllComments] = useState([]);
+  useEffect(() => {
+    const foundRestorants = allRestorants.filter(
+      (restoran) => restoran.id == paramsID
+    );
+    setDataSingleResturants(foundRestorants[0]);
+    setAllComments(foundRestorants[0].comments);
+  }, [allRestorants, paramsID]);
+
+  // useEffect(() => {
+  //   fetch(`${baseUrl}/restaurants/${paramsID.ResoruntID}`)
+  //     .then((res) => res.json())
+  //     .then((data) => setDataSingleResturants(data));
+  // }, []);
+
+  // useEffect(() => {
+  //   fetch(`${baseUrl}/restaurants/${paramsID.ResoruntID}/comments`)
+  //   .then(res => res.json())
+  //   .then(result => setAllComments(result))
+  // },[])
+  const [statusMenuShow, setStatusMenuShow] = useState("resturants-menu");
+  const [searchInMenuRestorant, setSearchInMenuRestorant] = useState([]);
+
+  const handleMenuSingleRestoranst = (e) => {
+    let filredMenu = dataSingleResturants.menu.filter((menus) =>
+      menus.name.includes(e.target.value)
+    );
+    setSearchInMenuRestorant(filredMenu);
+  };
+
+  // ==================    Handle User Basket ================
+
+  const [arrayUserBasket, setArrayUserBasket] = useState(() => {
+    const stored = localStorage.getItem("basket");
+    return stored ? JSON.parse(stored) : [];
+  });
+
+  const addToBasketUser = (foods) => {
+    setArrayUserBasket((prev) => {
+      let exist = prev.find((item) => item.id == foods.id);
+      if (exist) {
+        return prev.map((items) =>
+          items.id === foods.id ? { ...items, count: items.count + 1 } : items
+        );
+      } else {
+        return [...prev, { ...foods, count: 1 }];
+      }
+    });
+  };
+
+  // ===================================================================
+
+  // =============  Calculator User Basket  =============================
+
+  const [fainalyAllPriceFoods, setFainalyAllPriceFoods] = useState(0);
+
+  function CalculatorUserBasket() {
+    const allPriceFoods = arrayUserBasket?.map(
+      (item) => item.price * item.count
+    );
+    const resultAllPriceFoods = allPriceFoods?.reduce(
+      (acc, num) => acc + num,
+      0
+    );
+    setFainalyAllPriceFoods(resultAllPriceFoods);
+  }
+
+  useEffect(() => {
+    CalculatorUserBasket();
+  }, [arrayUserBasket]);
+
+  useEffect(() => {
+    localStorage.setItem("basket", JSON.stringify(arrayUserBasket));
+  }, [arrayUserBasket]);
+
+  // ====================================================================
+
+  // ===========    Delete Foods In UserBasket  =========================
+  // const [afterDeleteFoods, setAfterDeleteFoods] = useState();
+  const deleteFoodInUserBasket = (foodID) => {
+    const currentBasket = JSON.parse(localStorage.getItem("basket")) || [];
+
+    const updatedLocalStorageAfterDelete = currentBasket.filter(
+      (item) => item.id !== foodID
+    );
+    // setAfterDeleteFoods(updatedLocalStorageAfterDelete);
+    localStorage.setItem(
+      "basket",
+      JSON.stringify(updatedLocalStorageAfterDelete)
+    );
+    setArrayUserBasket(updatedLocalStorageAfterDelete);
+  };
+  // ====================================================================
+  // ====================================================================
+  const [mainCategury, setMainCategury] = useState("ساندویچ حرفه ای");
+
+  // ====================================================================
+  // ========================================================= ===========
+
+  const [isActiveCateguryFoods, setIsActiveCateguryFoods] = useState(false);
+
+  const wrapperCateguryFoods = useRef(null);
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 450) {
+      setIsActiveCateguryFoods(true)
+    }
+    else{
+      setIsActiveCateguryFoods(false)
+    }
+    // else {
+    //   console.log("");
+
+    // }
+  });
+
+  // ====================================================================
+
+
+
+  const [idProductInBasket, setIdProductInBasket] = useState();
+
+  return (
+    <div className="">
+      <Topbar
+        arrayUserBasket={arrayUserBasket}
+        setArrayUserBasket={setArrayUserBasket}
+        CalculatorUserBasket={CalculatorUserBasket}
+        addToBasketUser={addToBasketUser}
+        setIdProductInBasket={setIdProductInBasket}
+        deleteFoodInUserBasket={deleteFoodInUserBasket}
+        fainalyAllPriceFoods={fainalyAllPriceFoods}
+      />
+      <HeaderRestorant
+        dataSingleResturants={dataSingleResturants}
+        allComments={allComments}
+      />
+      <div className="flex flex-col bg-white justify-between pt-5 pr-4 pl-4 xs:pr-12 xs:pl-12">
+        <div className="w-[20rem]  ">
+          <ul className="flex  & > *:cursor-pointer justify-between pt-5 pr-3 pl-3 pb-3 items-center ">
+            <MenuSinglePageRestorant
+              setStatusMenuShow={setStatusMenuShow}
+              statusMenuShow={statusMenuShow}
+              keys="resturants-menu"
+              value=""
+              active
+              title="منوی رستوران"
+            />
+            <MenuSinglePageRestorant
+              setStatusMenuShow={setStatusMenuShow}
+              statusMenuShow={statusMenuShow}
+              keys="resturants-comments"
+              value=""
+              title="نظرات کاربران"
+            />
+            <MenuSinglePageRestorant
+              setStatusMenuShow={setStatusMenuShow}
+              statusMenuShow={statusMenuShow}
+              keys="resturants-infos"
+              value=""
+              title="اطلاعات کلی"
+            />
+          </ul>
+        </div>
+      </div>
+
+      <div
+        className={` ${
+          isActiveCateguryFoods ? "opacity-100 visible z-20  top-12 sm:top-14 fixed flex transition-all" : "opacity-0 invisible hidden "
+        } w-full shadow-2xs`}
+      >
+        <div className=" flex w-full border-solid shadow-2xl    items-center justify-center pt-3 bg-white">
+          <div className="  mx-auto flex items-center  overflow-x-scroll hide-scrollbar justify-center gap-14">
+            {categuryTitleFoods.categuryTitleFoods.map((item) => (
+              <div className="">
+                <SliderTitleFoods
+                  mainCategury={mainCategury}
+                  setMainCategury={setMainCategury}
+                  item={item}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {statusMenuShow == "resturants-comments" ? (
+        <CommentsSections
+          dataSingleResturants={dataSingleResturants}
+          allComments={allComments}
+        />
+      ) : null}
+      {statusMenuShow == "resturants-menu" ? (
+        <SinglePageRestorantMenu
+          arrayUserBasket={arrayUserBasket}
+          setArrayUserBasket={setArrayUserBasket}
+          idProductInBasket={idProductInBasket}
+          addToBasketUser={addToBasketUser}
+          searchInMenuRestorant={searchInMenuRestorant}
+          handleMenuSingleRestoranst={handleMenuSingleRestoranst}
+          dataSingleResturants={dataSingleResturants}
+        />
+      ) : null}
+      {statusMenuShow == "resturants-infos" ? (
+        <AddressRestorant dataSingleResturants={dataSingleResturants} />
+      ) : null}
+
+      <div className="mt-2">
+        <FooterPc />
+      </div>
+    </div>
+  );
+}
